b4008ac0cfdc757a041151df177a3099cce60038
exarobibliologist <exarobibliologist@gmail.com>
2020-03-07 19:14:30 -0500

    Tweaked the blocks generation and visual properties

--- mario/nodes.lua
+++ mario/nodes.lua
@@ -2,35 +2,7 @@ minetest.register_node("mario:block", {
 	description = "Block",
 	tiles = {"block1.png"},
 	is_ground_content = true,
-	groups = {oddly_breakable_by_hand=1, cracky=3, stone=1},
-	drop = {
-		max_items = 1,
-		items = {
-			{ items = {'mario:cloud'}, rarity = 2 },
-			{ items = {'mario:pwing'}, rarity = 2 },
-			{ items = {'mario:coin'}, rarity = 2 },
-			{ items = {'mario:mushroom'}, rarity = 2 },
-			{ items = {'mario:fireflower'}, rarity = 2 },
-			{ items = {'default:wood'}, rarity = 3 },
-			{ items = {'default:cobble'}, rarity = 3 },
-			{ items = {'default:sand'}, rarity = 3 },
-			{ items = {'default:tree'}, rarity = 4 },
-			{ items = {'default:iron_lump'}, rarity = 4 },
-			{ items = {'default:copper_lump'}, rarity = 4 },
-			{ items = {'default:coal_lump'}, rarity = 4 },
-			{ items = {'default:mese_crystal'}, rarity = 5 },
-			{ items = {'default:obsidian'}, rarity = 5 },
-			{ items = {'default:nyancat'}, rarity = 5 },
-			{ items = {'default:nyancat_rainbow'}, rarity = 5 }
-			}
-	} ,
-	legacy_mineral = true,
-})
-
-minetest.register_node("mario:exclamationblock", {
-	description = "Exclamation Block",
-	tiles = {"exclamationblock.png"},
-	is_ground_content = true,
+	light_source = 1,
 	groups = {oddly_breakable_by_hand=1, cracky=3, stone=1},
 	drop = {
 		max_items = 2,
@@ -48,9 +20,35 @@ minetest.register_node("mario:exclamationblock", {
 			{ items = {'default:copper_lump'}, rarity = 4 },
 			{ items = {'default:coal_lump'}, rarity = 4 },
 			{ items = {'default:mese_crystal'}, rarity = 5 },
-			{ items = {'default:obsidian'}, rarity = 5 },
-			{ items = {'default:nyancat'}, rarity = 5 },
-			{ items = {'default:nyancat_rainbow'}, rarity = 5 }
+			{ items = {'default:obsidian'}, rarity = 5 }
+			}
+	} ,
+	legacy_mineral = true,
+})
+
+minetest.register_node("mario:exclamationblock", {
+	description = "Exclamation Block",
+	tiles = {"exclamationblock.png"},
+	is_ground_content = true,
+	light_source = 2,
+	groups = {oddly_breakable_by_hand=1, cracky=3, stone=1},
+	drop = {
+		max_items = 4,
+		items = {
+			{ items = {'mario:cloud'}, rarity = 2 },
+			{ items = {'mario:pwing'}, rarity = 2 },
+			{ items = {'mario:coin'}, rarity = 2 },
+			{ items = {'mario:mushroom'}, rarity = 2 },
+			{ items = {'mario:fireflower'}, rarity = 2 },
+			{ items = {'default:wood'}, rarity = 3 },
+			{ items = {'default:cobble'}, rarity = 3 },
+			{ items = {'default:sand'}, rarity = 3 },
+			{ items = {'default:tree'}, rarity = 4 },
+			{ items = {'default:iron_lump'}, rarity = 4 },
+			{ items = {'default:copper_lump'}, rarity = 4 },
+			{ items = {'default:coal_lump'}, rarity = 4 },
+			{ items = {'default:mese_crystal'}, rarity = 5 },
+			{ items = {'default:obsidian'}, rarity = 5 }
 			}
 	} ,
 	legacy_mineral = true,
@@ -60,9 +58,10 @@ minetest.register_node("mario:question1", {
 	description = "Question Block",
 	tiles = {"questionblock1.png"},
 	is_ground_content = true,
+	light_source = 3,
 	groups = {oddly_breakable_by_hand=1, cracky=3, stone=1},
 	drop = {
-		max_items = 3,
+		max_items = 8,
 		items = {
 			{ items = {'mario:cloud'}, rarity = 2 },
 			{ items = {'mario:pwing'}, rarity = 2 },
@@ -77,9 +76,7 @@ minetest.register_node("mario:question1", {
 			{ items = {'default:copper_lump'}, rarity = 4 },
 			{ items = {'default:coal_lump'}, rarity = 4 },
 			{ items = {'default:mese_crystal'}, rarity = 5 },
-			{ items = {'default:obsidian'}, rarity = 5 },
-			{ items = {'default:nyancat'}, rarity = 5 },
-			{ items = {'default:nyancat_rainbow'}, rarity = 5 }
+			{ items = {'default:obsidian'}, rarity = 5 }
 			}
 	} ,
 	legacy_mineral = true,
@@ -89,9 +86,10 @@ minetest.register_node("mario:question2", {
 	description = "Question Block",
 	tiles = {"questionblock2.png"},
 	is_ground_content = true,
+	light_source = 4,
 	groups = {oddly_breakable_by_hand=1, cracky=3, stone=1},
 	drop = {
-		max_items = 3,
+		max_items = 16,
 		items = {
 			{ items = {'mario:cloud'}, rarity = 2 },
 			{ items = {'mario:pwing'}, rarity = 2 },
@@ -106,9 +104,7 @@ minetest.register_node("mario:question2", {
 			{ items = {'default:copper_lump'}, rarity = 4 },
 			{ items = {'default:coal_lump'}, rarity = 4 },
 			{ items = {'default:mese_crystal'}, rarity = 5 },
-			{ items = {'default:obsidian'}, rarity = 5 },
-			{ items = {'default:nyancat'}, rarity = 5 },
-			{ items = {'default:nyancat_rainbow'}, rarity = 5 }
+			{ items = {'default:obsidian'}, rarity = 5 }
 			}
 	} ,
 	legacy_mineral = true,
--- mario/tools.lua
+++ mario/tools.lua
@@ -22,11 +22,11 @@ minetest.register_tool("mario:feather_pick", {
 		full_punch_interval = 1.0,
 		max_drop_level=3,
 		groupcaps={
-			cracky={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=40, maxlevel=3},
-			crumbly={times={[1]=0.95, [2]=0.65, [3]=0.35}, uses=40, maxlevel=3},
-			choppy={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=40, maxlevel=3},
-			fleshy={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=40, maxlevel=3},
-			snappy={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=40, maxlevel=3}
+			cracky={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=20, maxlevel=3},
+			crumbly={times={[1]=0.95, [2]=0.65, [3]=0.35}, uses=20, maxlevel=3},
+			choppy={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=20, maxlevel=3},
+			fleshy={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=20, maxlevel=3},
+			snappy={times={[1]=0.80, [2]=0.60, [3]=0.40}, uses=20, maxlevel=3}
 		}
 	},
 })
